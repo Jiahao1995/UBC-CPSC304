2a826c09ff5586f95481023b991dd8d914afda33
Jiahao1995 <lijiahao5269@outlook.com>
2019-11-03 12:34:55 -0800

    add database connection handler

---
+++ src/database/DatabaseConnectionHandler.java
@@ -0,0 +1,46 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionHandler {
+
+    private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String EXCEPTION_TAG = "[EXCEPTION]";
+    private static final String WARNING_TAG = "[WARNING]";
+
+    private Connection connection = null;
+
+    public DatabaseConnectionHandler() {
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public boolean login(String username, String password) {
+        try {
+            if (connection != null)
+                connection.close();
+            connection = DriverManager.getConnection(ORACLE_URL, username, password);
+            connection.setAutoCommit(false);
+            System.out.println("\nConnected to Oracle!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            return false;
+        }
+    }
+
+}
